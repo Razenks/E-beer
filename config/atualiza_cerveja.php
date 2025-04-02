27cf766f610be716ec43de36da387e26494a235e
Rafhael rezende <137797639+Razenks@users.noreply.github.com>
2025-04-02 11:34:40 -0400

    Add files via upload

---
+++ config/atualiza_cerveja.php
@@ -0,0 +1,111 @@
+<?php
+require_once 'conectaBD.php';
+
+if (!empty($_POST)) {
+    session_start();
+    try {
+        // Captura o ID da cerveja que será atualizada
+        $id_cerveja = $_POST['id_cerveja'];
+
+        // Pega as informações do arquivo enviado (imagem)
+        $fileTmpPath = $_FILES['img_cerveja']['tmp_name'];
+        $fileName = $_FILES['img_cerveja']['name'];
+        $uploadDir = '../uploads/';
+        $destPath = $uploadDir . time() . '-' . $fileName;
+
+        // Verifica se há uma nova imagem para atualizar
+        if (!empty($fileTmpPath)) {
+            // Move o arquivo temporário para a pasta assets
+            if (move_uploaded_file($fileTmpPath, $destPath)) {
+                // Atualiza o caminho da imagem no banco de dados
+                $sql = 'UPDATE img_cerveja 
+                        SET img_cerveja = :img_cerveja 
+                        WHERE id_img_cerveja = (SELECT id_img_cerveja FROM cerveja WHERE id_cerveja = :id_cerveja)';
+                $stmt = $conexao->prepare($sql);
+                $stmt->execute([':img_cerveja' => $destPath, ':id_cerveja' => $id_cerveja]);
+            } else {
+                header('Location: ../pages/register_beer.php?msgError=Erro ao mover o arquivo para a pasta assets');
+                exit();
+            }
+        }
+
+        // Captura os outros dados do formulário
+        $nome = $_POST['nome'];
+        $desc_cerv = $_POST['descricao'];
+        $desc_cor = $_POST['cor'];
+        $desc_teor = $_POST['teor'];
+        $desc_amargor = $_POST['amargor'];
+        $desc_corpo = $_POST['corpo'];
+        $desc_aroma = $_POST['aroma'];
+        $desc_sabor = $_POST['sabor'];
+        $desc_carbonacao = $_POST['carbonacao'];
+        $desc_mouthfeel = $_POST['mouthfeel'];
+
+        // Função para obter IDs a partir das descrições
+        function getId($conexao, $tabela, $campo_desc, $descricao, $id_tabela)
+        {
+            $query = "SELECT $id_tabela FROM $tabela WHERE $campo_desc = :descricao";
+            $stmt = $conexao->prepare($query);
+            $stmt->bindParam(':descricao', $descricao);
+            $stmt->execute();
+            return $stmt->fetchColumn();
+        }
+
+        // Obtendo IDs
+        $id_cor = getId($conexao, 'cor', 'desc_cor', $desc_cor, 'id_cor');
+        $id_amargor = getId($conexao, 'amargor', 'desc_amargor', $desc_amargor, 'id_amargor');
+        $id_teor = getId($conexao, 'teor_alcoolico', 'desc_teor', $desc_teor, 'id_teor');
+        $id_corpo = getId($conexao, 'corpo_cerveja', 'desc_corpo', $desc_corpo, 'id_corpo');
+        $id_aroma = getId($conexao, 'aroma', 'desc_aroma', $desc_aroma, 'id_aroma');
+        $id_sabor = getId($conexao, 'sabor_principal', 'desc_sabor', $desc_sabor, 'id_sabor');
+        $id_carbonacao = getId($conexao, 'carbonatacao', 'desc_carbona', $desc_carbonacao, 'id_carbonatacao');
+        $id_mouthfeel = getId($conexao, 'mouthfeel', 'desc_mouthfeel', $desc_mouthfeel, 'id_mouthfeel');
+
+        // Atualizando as informações da cerveja no banco de dados
+        $sql = "UPDATE cerveja 
+                SET nome = :nome, 
+                    descricao = :descricao, 
+                    id_cor = :id_cor, 
+                    id_amargor = :id_amargor, 
+                    id_teor = :id_teor, 
+                    id_corpo = :id_corpo, 
+                    id_aroma = :id_aroma, 
+                    id_sabor = :id_sabor, 
+                    id_carbonatacao = :id_carbonacao, 
+                    id_mouthfeel = :id_mouthfeel 
+                WHERE id_cerveja = :id_cerveja";
+
+        $stmt = $conexao->prepare($sql);
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':descricao', $desc_cerv);
+        $stmt->bindParam(':id_cor', $id_cor);
+        $stmt->bindParam(':id_amargor', $id_amargor);
+        $stmt->bindParam(':id_teor', $id_teor);
+        $stmt->bindParam(':id_corpo', $id_corpo);
+        $stmt->bindParam(':id_aroma', $id_aroma);
+        $stmt->bindParam(':id_sabor', $id_sabor);
+        $stmt->bindParam(':id_carbonacao', $id_carbonacao);
+        $stmt->bindParam(':id_mouthfeel', $id_mouthfeel);
+        $stmt->bindParam(':id_cerveja', $id_cerveja);
+
+        if ($stmt->execute()) {
+            header("Location: ../pages/main_admin.php?msgSucessoUpdateBeer=Cerveja atualizada com sucesso!");
+            exit();
+        } else {
+            $errorInfo = $stmt->errorInfo();
+            echo "<pre>";
+            print_r($errorInfo);
+            echo "</pre>";
+            exit();
+        }
+
+    } catch (PDOException $e) {
+        echo "Erro: " . $e->getMessage();
+        echo "<br>";
+        echo "Código do erro: " . $e->getCode();
+    }
+} else {
+    header("Location: ../pages/cadastro.php?msgErro=Erro de Acesso.");
+    exit(); // Finaliza o script após o redirecionamento
+}
+?>
---
+++ pages/main_logado.php
@@ -0,0 +1,205 @@
+<?php
+session_start();
+
+// Verifica se o usuário está logado e tem um CPF armazenado
+if (!isset($_SESSION['cpf'])) {
+    header("Location: index.php?msgErro=Você precisa se autenticar no sistema.");
+    exit();
+}
+
+require_once '../config/conectaBD.php';
+
+// Consulta para obter informações do usuário com base no CPF
+$sql_usuario = "SELECT u.nome, u.email, u.senha, u.sobrenome, u.cpf, u.telefone, ft.img_foto_usuario
+                FROM usuario AS u
+                INNER JOIN 
+                    foto_usuario AS ft ON u.id_foto_usuario = ft.id_foto_usuario
+                WHERE cpf = :cpf";
+
+try {
+    // Prepara e executa a consulta para buscar as cervejas
+    $sql_cervejas = "SELECT cerveja.id_cerveja, cerveja.nome, img_cerveja.img_cerveja 
+                     FROM cerveja
+                     INNER JOIN img_cerveja ON cerveja.id_img_cerveja = img_cerveja.id_img_cerveja";
+    $stmt_cervejas = $conexao->prepare($sql_cervejas);
+    $stmt_cervejas->execute();
+    $cervejas = $stmt_cervejas->fetchAll(PDO::FETCH_ASSOC);
+
+    // Prepara e executa a consulta para buscar os dados do usuário com base no CPF
+    $stmt_usuario = $conexao->prepare($sql_usuario);
+    $stmt_usuario->bindParam(':cpf', $_SESSION['cpf']);
+    $stmt_usuario->execute();
+    $usuario = $stmt_usuario->fetch(PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+    echo "Erro ao buscar dados: " . $e->getMessage();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-beer</title>
+    <link rel="stylesheet" href="../css/main_logado.css">
+    <link rel="stylesheet" href="../css/all.css">
+    <link rel="stylesheet" href="../css/acessibilidade.css">
+    <script src="../js/acessibilidade.js"></script>
+    <script src="../js/main-novo.js"></script>
+    <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+    <script>new window.VLibras.Widget('https://vlibras.gov.br/app');</script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+</head>
+
+<body>
+    <div id="acessibilidade">
+        <div id="fonte">
+            <div>Tamanho da fonte: </div>
+            <button class="alt-acess" onclick="diminuirFonte()">A-</button>
+            <button class="alt-acess" onclick="resetarFonte()">A</button>
+            <button class="alt-acess" onclick="aumentarFonte()">A+</button>
+        </div>
+
+        <div id="cor">
+            <div>Cor:</div>
+            <button class="alt-acess" onclick="voltarModoOriginal()"><img src="../assets/sol.png" alt=""></button>
+            <button class="alt-acess" onclick="alternarModoNoturno()"><img src="../assets/lua.png" alt=""></button>
+        </div>
+    </div>
+
+    <div vw class="enabled">
+        <div vw-access-button class="active"></div>
+        <div vw-plugin-wrapper>
+            <div class="vw-plugin-top-wrapper"></div>
+        </div>
+    </div>
+
+    <header>
+        <nav id="navegation-bar">
+            <div id="logo-image">
+                <a href="main_logado.php"><img src="../assets/logo_ebeer.png" alt="logo_ebeer"
+                        style="width: 110px; height: 50px;"></a>
+            </div>
+            <div id="profile-config">
+                <img src="../assets/icons8-male-user-96.png" alt="" style="width: 50px; height: 50px;">
+                <div id="name-account">
+                    <p>Olá, <?php echo $_SESSION['nome']; ?></p>
+                    <button id="account">Conta</button>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <section id="account-details" style="display: none;">
+        <img src="../assets/botaox.png" alt="" id="button-close">
+        <!-- Mostrar a foto do usuário, se disponível -->
+        <img src="<?php echo $usuario['img_foto_usuario']; ?>" alt="Foto de perfil" id="profile-photo">
+
+        <h1 id="titulo-account"><?php echo $_SESSION['nome'] . ' ' . $_SESSION['sobrenome']; ?></h1>
+
+        <p class="email-details"><?php echo $_SESSION['email']; ?></p>
+
+        <p class="phone-details"><?php echo !empty($usuario['telefone']) ? $usuario['telefone'] : ''; ?></p>
+
+        <button id="change-data" class="change-data">Alterar meus dados</button>
+
+        <button id="button-quit">
+            <a href="../config/logout.php">
+                <div id="img-quit"><img src="../assets/icons8-desligar-52 (1).png" alt=""></div>
+                <div>Sair</div>
+            </a>
+        </button>
+    </section>
+
+    <section id="change-data-details" style="display: none;">
+        <img src="../assets/botaox.png" alt="" id="button-back-details">
+        <form action="../config/alterar_dados.php" method="POST" id="change-data-inputs" enctype="multipart/form-data">
+            <label for="picture_input" class="picture" tabindex="0">
+                <img id="picture_preview" alt="Pré-visualização da foto" class="picture_img">
+                <input type="file" accept="image/*" id="picture_input" name="foto">
+            </label>
+
+
+            <div>
+                <label for="nome">Nome: </label>
+                <input type="text" name="nome" value="<?php echo $_SESSION['nome'] ?>">
+            </div>
+            <div>
+                <label for="sobrenome">Sobrenome: </label>
+                <input type="text" name="sobrenome" value="<?php echo $_SESSION['sobrenome'] ?>">
+            </div>
+            <div>
+                <label for="email">Email: </label>
+                <input type="text" name="email" value="<?php echo $_SESSION['email'] ?>">
+            </div>
+            <div>
+                <label for="cpf">CPF: </label>
+                <input type="text" name="cpf" value="<?php echo $_SESSION['cpf'] ?>" readonly>
+            </div>
+            <div>
+                <label for="telefone">Telefone: </label>
+                <input type="text" name="telefone" id="change-telefone"
+                    value="<?php echo !empty($usuario['telefone']) ? $usuario['telefone'] : ''; ?>" maxlength="15">
+            </div>
+            <button type="submit" id="save-data-changes">Salvar Alterações</button>
+        </form>
+    </section>
+
+    <section id="first-section">
+        <h1 id="titulo-beer-test">Encontrar sua cerveja perfeita <br> nunca foi tao fácil</h1>
+        <a href="beer_test.php" id="beer-test"><button>BeerFeed</button></a>
+    </section>
+
+    <section id="second-section">
+        <h1>Produtos</h1>
+        <button id="button-back"><img src="../assets/icons8-voltar-72.png" alt=""></button>
+        <button id="button-advance" type="button"><img src="../assets/icons8-avançar-72.png" alt=""></button>
+
+        <div id="second-section-images">
+            <?php
+            if (!empty($cervejas)) {
+                foreach ($cervejas as $cerveja) {
+                    echo '<div>';
+                    echo '<div>';
+                    echo '<img src="' . $cerveja['img_cerveja'] . '" alt="' . $cerveja['nome'] . '">';
+                    echo '<p>' . $cerveja['nome'] . '</p>';
+                    echo '</div>';
+                    echo '<a href="beer_page.php?id_cerveja=' . $cerveja['id_cerveja'] . '">Ver Detalhes</a>';
+                    echo '</div>';
+                }
+            } else {
+                echo '<p>Nenhuma cerveja cadastrada.</p>';
+            }
+            ?>
+        </div>
+    </section>
+
+    <section id="third-section">
+        <h2>Como funciona o e-Beer?</h2>
+        <img src="../assets/canecas.png" alt="">
+        <p id="third-section-text">Com o e-beer, você pode responder ao nosso questionário para encontrar a cerveja
+            ideal para o seu paladar. Além disso, você pode explorar diversas opções em nosso catálogo de produtos.
+        </p>
+    </section>
+    </main>
+
+    <div id="imgAcess">
+        <img src="../assets/acess.png" alt="" onclick="alterarAcessibilidade()">
+    </div>
+
+    <!-- Rodapé da página -->
+    <footer id="footer">
+        <div id="last-section">
+            <!-- Link para voltar ao topo da página -->
+            <a href="">Voltar ao topo</a>
+        </div>
+        <!-- Nome da marca no rodapé -->
+        <p>e-Beer</p>
+    </footer>
+</body>
+
+</html>
