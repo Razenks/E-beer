1e4db5b643b24cc9c97ed8813e4f92f00ccb16bf
Rafhael rezende <137797639+Razenks@users.noreply.github.com>
2025-04-02 11:32:46 -0400

    Delete includes directory

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-<!-- Rodapé da página -->
-<footer id="footer">
-    <div id="last-section">
-        <!-- Link para voltar ao topo da página -->
-        <a href="">Voltar ao topo</a>
-    </div>
-    <!-- Nome da marca no rodapé -->
-    <p>e-Beer</p>
-</footer>
